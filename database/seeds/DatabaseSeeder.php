0092f2c8147478ae2530202ed064eb28052739c6
devjk1 <vagrant@homestead>
2020-07-07 17:06:00 +0000

    db setup, seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        // $threads = factory(App\Thread::class, 100)->create();
+        // $threads->each(function ($thread) {
+        //     $thread->replies()->saveMany(factory(App\Reply::class, 10)->make(['thread_id' => $thread]));
+        // });
+        
+        $threads = factory(App\Thread::class, 10)->create();
+        foreach($threads as $thread) {
+            $users = $threads->map(function ($thread) {
+                return $thread->user;
+            });
+            foreach($users as $user) {
+                $user->replies()->save(factory(App\Reply::class)->make(['user_id' => $user, 'thread_id' => $thread]));
+            }
+        }
     }
 }
